03f3d6dcfe3c27ff086ff4e68a6e62321bfa5132
Henriqueprim <henrique_salmprim@hotmail.com>
2022-10-21 16:48:41 -0300

    chore: creates models

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,42 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+export default User;
